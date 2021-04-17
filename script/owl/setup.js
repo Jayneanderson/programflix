a5a448738d068bb0a3a012a9878e64ee6cf8dc67
Jayneanderson <jayneanderson.santos@gmail.com>
2021-04-17 01:00:10 -0300

    Melhorando o tamanho dos cars

--- script/owl/setup.js
+++ script/owl/setup.js
@@ -5,13 +5,13 @@ $('.owl-carousel').owlCarousel({
     nav: false, /* botões de navegação */
     responsive: {
         0: {
-            items: 1
-        },
-        600: {
             items: 3
         },
+        600: {
+            items: 4,
+        },
         1000: {
-            items: 5
+            items: 7
         }
     }
 })
